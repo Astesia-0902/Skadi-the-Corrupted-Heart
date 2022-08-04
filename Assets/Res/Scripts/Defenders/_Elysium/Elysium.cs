794e52aec533a4a2175439308f9ba1003c3a075d
奇迹的基斯卡 <a171895102@hotmail.com>
2022-08-04 15:57:57 +0200

    完成了极境的技能

--- Assets/Res/Scripts/Defenders/_Elysium/Elysium.cs
+++ Assets/Res/Scripts/Defenders/_Elysium/Elysium.cs
@@ -1,9 +1,38 @@
-using Res.Scripts.Defenders;
+using UnityEngine;
 
-namespace Defenders._Elysium
+namespace Res.Scripts.Defenders._Elysium
 {
     public class Elysium : Defender
     {
-    
+        private bool isSkillOn;
+        private float skillTimer;
+        public GameObject skillEffect;
+        private static readonly int SkillEnd = Animator.StringToHash("skillEnd");
+
+        protected override void Update()
+        {
+            base.Update();
+            SkillTimerUpdate();
+        }
+
+        public void CastSkill()
+        {
+            isSkillOn = true;
+            animatorManager.PlayTargetAnimation("Skill_1_Start", true);
+        }
+
+        public void SkillTimerUpdate()
+        {
+            if (!isSkillOn)
+                return;
+            
+            skillTimer += Time.deltaTime;
+            if (skillTimer >= 8f)
+            {
+                skillTimer = 0f;
+                isSkillOn = false;
+                animatorManager.anim.SetTrigger(SkillEnd);
+            }
+        }
     }
 }
